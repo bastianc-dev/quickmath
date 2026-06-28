b67ef84536cc37855adc7de586ee574a94505288
bastianc-dev <bastianc.dev@gmail.com>
2026-06-28 00:05:08 -0400

    Input automático

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+// =========================
+// Cambiar opciones de números
+// =========================
+
 const operaciones = document.querySelectorAll('input[name="operacion"]');
 
 const opcion1 = document.getElementById("opcion1");
@@ -29,18 +33,76 @@ operaciones.forEach(radio => {
 
 actualizarNumeros();
 
-const menu = document.getElementById("menuScreen");
-const game = document.getElementById("gameScreen");
+// =========================
+// Cambio de pantalla
+// =========================
+
+const menuScreen = document.getElementById("menuScreen");
+const gameScreen = document.getElementById("gameScreen");
 
 const btnInfinite = document.getElementById("btnInfinite");
 const btnTimer = document.getElementById("btnTimer");
 
+// =========================
+// Operaciones de prueba
+// =========================
+
+const operationElement = document.getElementById("operation");
+const answerInput = document.getElementById("answer");
+
+const operations = [
+    {
+        text: "34 + 29",
+        answer: "63"
+    },
+    {
+        text: "7 + 8",
+        answer: "15"
+    }
+];
+
+let currentOperation = 0;
+
+function showOperation() {
+
+    operationElement.textContent = operations[currentOperation].text;
+
+    answerInput.value = "";
+    answerInput.focus();
+
+}
+
 function iniciarJuego() {
 
-    menu.style.display = "none";
-    game.style.display = "flex";
+    menuScreen.style.display = "none";
+    gameScreen.style.display = "flex";
+
+    showOperation();
 
 }
 
 btnInfinite.addEventListener("click", iniciarJuego);
 btnTimer.addEventListener("click", iniciarJuego);
+
+// =========================
+// Comprobación automática
+// =========================
+
+answerInput.addEventListener("input", () => {
+
+    // Solo permite números
+    answerInput.value = answerInput.value.replace(/\D/g, "");
+
+    if (answerInput.value === operations[currentOperation].answer) {
+
+        currentOperation++;
+
+        if (currentOperation >= operations.length) {
+            currentOperation = 0;
+        }
+
+        showOperation();
+
+    }
+
+});
